6e814d425e96f55e9fcd19e8e46bf90e68b173e3
imanmansour86 <imanmansour86@gmail.com>
2020-12-23 21:41:19 -0800

    fixing search bookshelf

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
  MyReads project is a bookshelf app that allows you to select and categorize books you have read, are currently reading, or want to read. The project emphasizes using React to build the application and provides an API server and client library that you will use to persist information as you interact with the application. This project is the final section in Learning React Fundamentals for Udacity React nanodegree. 
 
-## TL;DR
 
-To get started developing right away:
+
+To run the app right away:
 
 * install all project dependencies with `npm install`
 * start the development server with `npm start`
--- src/Search.js
+++ src/Search.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 class Search extends Component {
+    static propTypes = {
+        books: PropTypes.any.isRequired,
+    }
+
     state = {
         query: '',
         searchResults: [],
@@ -19,9 +24,18 @@ class Search extends Component {
     searchBook = (query) => {
         BooksAPI.search(query)
             .then(
-                (searchResults) => {
-                    console.log(searchResults)
-                    if (searchResults !== undefined && searchResults.error !== "empty query") {
+                (filteredBooks) => {
+                    console.log(filteredBooks)
+                    if (filteredBooks !== undefined && filteredBooks.error !== "empty query") {
+                        const searchResults = filteredBooks.map((book) => {
+                            const foundBookWithShelf = this.props.books.find(bookToBeFound => book.id === bookToBeFound.id)
+                            if (foundBookWithShelf) 
+                            book.shelf = foundBookWithShelf.shelf
+                            else 
+                            book.shelf = 'none'
+                            return book;
+                        })
+
                         this.setState(
                             () => ({
                                 searchResults
@@ -58,14 +72,6 @@ class Search extends Component {
                 <div className="search-books-bar">
                     <Link to='/' className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
 
                         <input type="text" placeholder="Search by title or author"
                             value={query}
@@ -77,8 +83,8 @@ class Search extends Component {
 
                 <div className="search-books-results">
                     <ol className="books-grid">
-
-                        {searchResults.map((book) => <Book key={book.id} book={book} onChange={this.handleSearchChange} />)
+                        {
+                            searchResults.map((book) => <Book key={book.id} book={book} onChange={this.handleSearchChange} />)
                         }
 
                     </ol>
